5bb51a3a20c0b68e0bc152837b781f1ced11a118
Robert Smith <thersmith09@gmail.com>
2025-08-21 22:06:43 -0700

    Working pomodoro

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,32 +1,85 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../styles/Pomodoro.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faPause, faRefresh } from "@fortawesome/free-solid-svg-icons";
 
 const Pomodoro = () => {
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timeRemaining, setTimeRemaining] = useState(0);
-  const [progressStep, setProgressStep] = useState(0);
-
   const steps = ["Working", "Short Break", "Long Break", "Done"];
+  const workTime = 25 * 60;
+  const shortBreakTime = 5 * 60;
+  const longBreakTime = 30 * 60;
+  const cyclesRemaining = 4;
+  
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(workTime);
+  const [progressStep, setProgressStep] = useState("Working");
+  const [cycleCount, setCycleCount] = useState(0);
+
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    if (timerStarted) {
+        intervalRef.current = setInterval(() => {
+            setTimeRemaining(prev => {
+                if (prev <= 1) {
+                    handleCompletedStep();
+                    return 0;
+                }
+                return prev - 1;
+            });
+        }, 1000);
+    }
+    return () => clearInterval(intervalRef.current);
+  }, [timerStarted]);
+
+  const handleCompletedStep = () => {
+    clearInterval(intervalRef.current);
+    setTimerStarted(false);
+
+    if (progressStep === "Working") {
+        const newCycleCount = cycleCount + 1;
+        setCycleCount(newCycleCount);
+
+        if (newCycleCount % cyclesRemaining === 0) {
+            setProgressStep("Long Break");
+            setTimeRemaining(longBreakTime);
+        }
+        else {
+            setProgressStep("Short Break");
+            setTimeRemaining(shortBreakTime);
+        }
+    }
+    else {
+            setProgressStep("Working");
+            setTimeRemaining(workTime);
+    }
+  };
+
+  const handleReset = () => {
+    clearInterval(intervalRef.current);
+    setTimerStarted(false);
+    setTimeRemaining(workTime);
+    setProgressStep("Working");
+    setCycleCount(0);
+  };
 
   return (
     <div className="pomodoro-container">
       <h1 className="text-4xl font-bold text-(--pomodoro-text)">Pomo</h1>
-      <p>{steps}</p>
+      <p>{progressStep}</p>
 
       <h2>Time Remaining:</h2>
       <div className="bg-(--pomodoro-secondary) p-4 rounded-lg ">
-        <h1 className="text-8xl font-bold">00:00</h1>
+        <h1 className="text-8xl font-bold">{Math.floor(timeRemaining / 60)}:{String(timeRemaining % 60).padStart(2, "0")}</h1>
       </div>
       <div className="button-group">
-        <button className="btn">
+        <button className="btn" onClick={() => setTimerStarted(true)}>
           <FontAwesomeIcon icon={faPlay} />
         </button>
-        <button className="btn">
+        <button className="btn" onClick={() => setTimerStarted(false)}>
           <FontAwesomeIcon icon={faPause} />
         </button>
-        <button className="reset-btn">
+        <button className="reset-btn" onClick={() => {handleReset()}}>
           <FontAwesomeIcon icon={faRefresh} />
         </button>
       </div>
